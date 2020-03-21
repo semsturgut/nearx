8844cb7d34e0be7e128d81a18acc91a035c84445
Sems Turgut <mtdetroit@gmail.com>
2020-03-21 03:48:09 +0300

    Circle adet sayisi guncellemede ios tarafinda sikinti var. + lar yapildi

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,17 @@ import 'package:geolocator/geolocator.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'dart:async';
 
+//TODO: Unique ID ile veri gonderimi yapilacak (firebase auth ile yapilacak) ?
+//TODO: Mavi vaka gönderiminde uyarı penceresi eklenecek ?
+//TODO: Mavi ve Kırmızı dairelerin uyarıları ekranda asılı olacak(harita üzerine kırmızı/mavi yazı) halkaların açıklaması ?
+
+//TODO: general diye bir collection acilacak ve icinde maksimum kisi onay sayisi gibi gerekli veriler barindiracak -
+//TODO: Olası vakalar Mavi Kesinleşmiş vakalar Kırmızı -
+//TODO: Lokasyonlar 2 digit olarak gonderilecek. +
+//TODO: Aynı lokasyondan gönderilen 5 adet bildirim Mavi Olası vaka olarak yayınlanacak +
+//TODO: Mavi lokasyonlarda haberlerde kanıtlanmış vakalar Kırmızı olarak değiştirilecek ?
+//TODO: Documents ismi firebase database de koordinat ile degistirilecek +
+
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
@@ -24,11 +35,14 @@ class MapSample extends StatefulWidget {
 class MapSampleState extends State<MapSample> {
   GoogleMapController _mapController;
   Firestore firestore = Firestore.instance;
+  List<DocumentSnapshot> circleLocations;
 
   Set<Circle> _circles = Set();
 
   static LatLng _initialPosition;
   static LatLng _lastPosition;
+
+  int generalNumber = 0;
 
   @override
   void initState() {
@@ -39,37 +53,54 @@ class MapSampleState extends State<MapSample> {
     });
   }
 
+//  Firebase'den halka loaksyonlarını topluyor
   void _getCircleLocation() async {
+    await firestore
+        .collection("general")
+        .document("maximumDataToBlueCircle")
+        .get()
+        .then((generalData) {
+      generalNumber = generalData.data["number"];
+    });
     await firestore
         .collection("locations")
         .getDocuments()
-        .then((QuerySnapshot snapshot) {
-      if (snapshot.documents.isNotEmpty)
-        for (int i = 0; i < snapshot.documents.length; i++) {
-          _setCircles(snapshot.documents.elementAt(i));
+        .then((QuerySnapshot locationsSnapshot) {
+      if (locationsSnapshot.documents.isNotEmpty) {
+        circleLocations = locationsSnapshot.documents;
+        for (int i = 0; i < circleLocations.length; i++) {
+          if (circleLocations.elementAt(i).data["numbercases"] >=
+                  generalNumber &&
+              generalNumber != 0) {
+            _setCircles(circleLocations.elementAt(i));
+          }
         }
+      }
     });
   }
 
+//  Halka lokasyonlarını alıp mapsde gosterme islemini yapiyor
   void _setCircles(DocumentSnapshot location) {
     setState(() {
       Circle resultCircle = Circle(
         circleId: CircleId(_circles.length.toString()),
         center: LatLng(location.data['latitude'], location.data['longitude']),
         radius: 500,
-        fillColor: Color.fromRGBO(150, 50, 50, .3),
+        fillColor: Color.fromRGBO(50, 50, 150, .3),
         strokeWidth: 1,
       );
       _circles.add(resultCircle);
     });
   }
 
+//  Maps kamera hareketi kontrolu
   void _onCameraMove(CameraPosition position) {
     setState(() {
       _lastPosition = _initialPosition;
     });
   }
 
+//  Kullanicinin anlik keskin lokasyon verisi
   void _getUserLocation() async {
     Position position = await Geolocator()
         .getCurrentPosition(desiredAccuracy: LocationAccuracy.high);
@@ -78,12 +109,14 @@ class MapSampleState extends State<MapSample> {
     });
   }
 
+//  Maps olusturulduktan sonra yapilacaklar bu fonksiyonda
   void _onMapCreated(GoogleMapController controller) {
     setState(() {
       _mapController = controller;
     });
   }
 
+//  Build ana tasarim elemanlari
   @override
   Widget build(BuildContext context) {
     return _initialPosition == null
@@ -126,7 +159,7 @@ class MapSampleState extends State<MapSample> {
                   child: FloatingActionButton(
                     onPressed: _addGeoPoint,
                     isExtended: true,
-                    backgroundColor: Colors.redAccent,
+                    backgroundColor: Colors.blueAccent,
                     child: Icon(Icons.add),
                   ),
                 )
@@ -135,13 +168,67 @@ class MapSampleState extends State<MapSample> {
           );
   }
 
-  Future<DocumentReference> _addGeoPoint() async {
+//  Kullanicinin bulundugu lokasyondaki vakayi firebase e ekleme
+  void _addGeoPoint() async {
+    // <--- ASYNC
+//    TODO: Butun datalarla latlong karsilastirmasi yapilacak +
+//    TODO: FIREBASE AUTH karsilastirmasi ile daha once ayni noktadan veri gondermemisse +1 artacak
 //    TODO: Change data type to geopoint.
-    return await firestore.collection('locations').add({
-      'latitude': _initialPosition.latitude,
-      'longitude': _initialPosition.longitude,
-      'hashcode': _initialPosition.hashCode,
-      'numbercases': 1
-    });
+//    TODO: Neden iki tane async var <--????-->
+    String queryName = _initialPosition.latitude.toStringAsFixed(2) +
+        '-_-' +
+        _initialPosition.longitude.toStringAsFixed(2);
+    if (circleLocations.contains(queryName)) {
+      for (int i = 0; i < circleLocations.length; i++) {
+        if (circleLocations.elementAt(i).documentID == queryName) {
+          await firestore.collection("locations").document(queryName).updateData({
+            'latitude':
+                double.parse(_initialPosition.latitude.toStringAsFixed(2)),
+            'longitude':
+                double.parse(_initialPosition.longitude.toStringAsFixed(2)),
+            'numbercases': circleLocations.elementAt(i).data['numbercases'] + 1
+          });
+        }
+      }
+    } else {
+      await firestore.collection("locations").document(queryName).setData({
+        'latitude': double.parse(_initialPosition.latitude.toStringAsFixed(2)),
+        'longitude':
+            double.parse(_initialPosition.longitude.toStringAsFixed(2)),
+        'numbercases': 1,
+      });
+    }
   }
 }
+
+//if (element.documentID == queryName) {
+//await firestore.collection("locations").document(queryName).setData({
+//'latitude':
+//double.parse(_initialPosition.latitude.toStringAsFixed(2)),
+//'longitude':
+//double.parse(_initialPosition.longitude.toStringAsFixed(2)),
+//'numbercases': element.data['numbercases'] + 1
+//});
+//} else {
+//await firestore.collection("locations").document(queryName).setData({
+//'latitude':
+//double.parse(_initialPosition.latitude.toStringAsFixed(2)),
+//'longitude':
+//double.parse(_initialPosition.longitude.toStringAsFixed(2)),
+//'numbercases': () {
+//if (element.documentID == queryName)
+//element.data['numbercases'] = element.data['numbercases'] + 1;
+//return element.data['numbercases'];
+//}
+//});
+//}
+
+//await firestore.collection("locations").document(queryName).setData({
+//'latitude': double.parse(_initialPosition.latitude.toStringAsFixed(2)),
+//'longitude': double.parse(_initialPosition.longitude.toStringAsFixed(2)),
+//'numbercases': () {
+//if (circleLocations.elementAt(i).documentID == queryName)
+//circleLocations.elementAt(i).data['numbercases'] = circleLocations.elementAt(i).data['numbercases'] + 1;
+//return circleLocations.elementAt(i).data['numbercases'];
+//}
+//});
